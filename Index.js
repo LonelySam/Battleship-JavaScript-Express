1bf15e491f40e005bf85a329c11a12859b00af9e
SoledadFernandez <soledad.fernandez@fundacion-jala.org>
2018-08-03 18:05:18 -0400

    CORS enabled

---
+++ Index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const Game = require('./src/Game.js')
+
+app.get('/game',(req, res) => {
+  Game.join(req.query.token)
+  .then(game => {
+      res.send(game)
+    })
+  .catch(error => console.error(error))
+})
+
+app.post('/game', (req, res) => {
+  Game.create(req.body)
+    .then(game => {
+      res.send(game)
+    })
+    .catch(error => console.error(error))
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
